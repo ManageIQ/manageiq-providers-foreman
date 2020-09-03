c20149e3b41d625d1b7a6ca17506d6864355021f
Dávid Halász <dhalasz@redhat.com>
2020-09-03 18:26:22 +0200

    Explicitly add missing IDs into DDF schemas

--- app/models/manageiq/providers/foreman/provider.rb
+++ app/models/manageiq/providers/foreman/provider.rb
@@ -32,17 +32,20 @@ class ManageIQ::Providers::Foreman::Provider < ::Provider
       :fields => [
         {
           :component => 'sub-form',
+          :id        => 'endpoints-subform',
           :name      => 'endpoints-subform',
           :title     => _("Endpoint"),
           :fields    => [
             {
               :component              => 'validate-provider-credentials',
+              :id                     => 'authentications.default.valid',
               :name                   => 'authentications.default.valid',
               :skipSubmit             => true,
               :validationDependencies => %w[type zone_id],
               :fields                 => [
                 {
                   :component  => "text-field",
+                  :id         => "endpoints.default.url",
                   :name       => "endpoints.default.url",
                   :label      => _("URL"),
                   :isRequired => true,
@@ -50,6 +53,7 @@ class ManageIQ::Providers::Foreman::Provider < ::Provider
                 },
                 {
                   :component    => "select",
+                  :id           => "endpoints.default.verify_ssl",
                   :name         => "endpoints.default.verify_ssl",
                   :label        => _("SSL verification"),
                   :isRequired   => true,
@@ -67,6 +71,7 @@ class ManageIQ::Providers::Foreman::Provider < ::Provider
                 },
                 {
                   :component  => "text-field",
+                  :id         => "authentications.default.userid",
                   :name       => "authentications.default.userid",
                   :label      => _("Username"),
                   :helperText => _("Should have privileged access, such as root or administrator."),
@@ -75,6 +80,7 @@ class ManageIQ::Providers::Foreman::Provider < ::Provider
                 },
                 {
                   :component  => "password-field",
+                  :id         => "authentications.default.password",
                   :name       => "authentications.default.password",
                   :label      => _("Password"),
                   :type       => "password",
